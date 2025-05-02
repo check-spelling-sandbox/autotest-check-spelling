2426c9ff12181fcd55e7170a4866a5511fb5dd79
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-01 22:54:57 -0400

    Add spell-check-this
    - Change workflow to use one line per item
    - Rewrite spelling metadata for spell-check-this repositories
    
    ---
    
    This could be repurposed to eventually add a check of just the spelling
    metadata in non spell-check-this repositories by:
    1. Adding another step that:
      1. empties `excludes.txt`
      2. adds an `only.txt` with the spelling metadata directory
    2. Adding another spelling step with:
        with `continue-on-error: true`
    
    It might also be possible to do all of this in a wrapper action so that
    the code just says:
    
    ```yml
    uses: check-spelling-sandbox/autotest-check-spelling/wrapper@branch
    with:
      ...
    ```
    
    And then that action takes steps like this:
    - normal cleanup
    - normal check-spelling
      possibly with a `continue-on-error`
    - spelling metadata cleanup
    - check-spelling of metadata
      with a `continue-on-error`
    - checks the results of the two check-spelling steps and reports that.

--- rewrite-workflow.pl
+++ rewrite-workflow.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/env perl
+my $repo = $ENV{repo};
+my $is_spell_check_this = $repo =~ m</spell-check-this>;
+my $set_name = 0;
+
 while (<>) {
   if (/^(\s*)steps:/) {
     $depth = $1;
@@ -12,11 +16,38 @@ while (<>) {
   } elsif ($mode == 2 && /^(\s+)\w.*:/) {
     $mode = 0;
   } elsif ($mode == 1 && /^(\s*)-/) {
-    print qq<$1- uses: actions/checkout\@v4\n$1  with:\n$1    repository: $ENV{repo}\n>;
+    my $indent = $1;
+    print qq<$indent- uses: actions/checkout\@v4\n$1  with:\n$1    repository: $repo>;
+    my $ignore_non_comment_lines = '$_ = "#\n" unless /^$|^# /';
+    my $action_dir = q<$(find .github/actions/ -mindepth 1 -maxdepth 1 -type d -name 'spell*')>;
+    if ($is_spell_check_this) {
+      my $replacement = '# $1';
+      my $code = qq?
+    ref: prerelease
+- name: enable checking spelling metadata
+  shell: bash
+  run: |
+    : Rewrite spelling metadata
+    perl -pi -e 's,(.*\.github/actions/spell),$replacement,' $action_dir/excludes.txt
+    find .github/actions/spell* -name '*patterns*' -o -name reject.txt |
+      xargs -r perl -pi -e '$ignore_non_comment_lines'
+    curl -fsSL https://raw.githubusercontent.com/$ENV{GITHUB_REPOSITORY}/$ENV{GITHUB_REF}/shims/$repo/.github/actions/spelling/allow.txt |
+      tee -a "$action_dir/allow.txt" > /dev/null || true
+    git diff || true
+?;
+      $code =~ s/^/$indent/gm;
+      print $code;
+    } else {
+      print "\n";
+    }
     $mode = 0;
   }
-  s/^(name: .*)/$1 ($ENV{repo})/;
+  $set_name = 1 if s/^(name: .*)/$1 ($repo)/;
   s/^(\s*cancel-in-progress:).*/$1 false/;
   s/(^\s+checkout:) true/$1 false/;
   print;
+}
+unless ($set_name) {
+  print;
+  print "name: ? Check Spelling ($repo)\n";
 }
