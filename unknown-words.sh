check-spelling/check-spelling

commit 2d44fce812639aef62fc025e9520c14be61b397a

Suggest candidate patterns

unknown-words.sh
@@ -792,6 +792,7 @@ define_variables() {
   dict="$spellchecker/words"
   patterns="$spellchecker/patterns.txt"
   forbidden_path="$spellchecker/forbidden.txt"
+  candidates_path="$spellchecker/candidates.txt"
   excludes="$spellchecker/excludes.txt"
   excludes_path="$temp/excludes.txt"
   only="$spellchecker/only.txt"
@@ -1213,6 +1214,7 @@ set_up_files() {
   fi
   should_exclude_file=$data_dir/should_exclude.txt
   counter_summary_file=$data_dir/counter_summary.json
+  candidate_summary="$data_dir/candidate_summary.txt"
   if [ "$INPUT_TASK" = 'spelling' ]; then
     get_project_files dictionary.words $dictionary_path
     get_project_files dictionary.txt $dictionary_path
@@ -1311,10 +1313,13 @@ set_up_files() {
       cp "$only_path" "$only"
     fi
     get_project_files line_forbidden.patterns $forbidden_path
+    get_project_files candidate.patterns "$candidates_path"
   fi
   extra_dictionaries_cover_entries=$(mktemp)
   get_project_files line_masks.patterns $patterns_path
   get_project_files patterns.txt $patterns_path
+  new_patterns_file="$append_to"
+  new_patterns_file_new="$append_to_generated"
   if [ -s "$patterns_path" ]; then
     cp "$patterns_path" "$patterns"
   fi
@@ -1480,8 +1485,24 @@ run_spell_check() {
   warning_output=$(mktemp -d)/warnings.txt
   more_warnings=$(mktemp)
   cat $file_list |\
-  env -i SHELL="$SHELL" PATH="$PATH" LC_ALL="C" HOME="$HOME" INPUT_LONGEST_WORD="$INPUT_LONGEST_WORD" INPUT_SHORTEST_WORD="$INPUT_SHORTEST_WORD" INPUT_LARGEST_FILE="$INPUT_LARGEST_FILE" xargs -0 -n$queue_size "-P$job_count" "$word_splitter" |\
-  expect="$expect_path" warning_output="$warning_output" more_warnings="$more_warnings" should_exclude_file="$should_exclude_file" counter_summary="$counter_summary_file" unknown_word_limit="$INPUT_UNKNOWN_WORD_LIMIT" "$word_collator" |\
+  env -i \
+    SHELL="$SHELL" \
+    PATH="$PATH" \
+    LC_ALL="C" \
+    HOME="$HOME" \
+    INPUT_LONGEST_WORD="$INPUT_LONGEST_WORD" \
+    INPUT_SHORTEST_WORD="$INPUT_SHORTEST_WORD" \
+    INPUT_LARGEST_FILE="$INPUT_LARGEST_FILE" \
+  xargs -0 -n$queue_size "-P$job_count" "$word_splitter" |\
+    expect="$expect_path" \
+    warning_output="$warning_output" \
+    more_warnings="$more_warnings" \
+    should_exclude_file="$should_exclude_file" \
+    counter_summary="$counter_summary_file" \
+    unknown_word_limit="$INPUT_UNKNOWN_WORD_LIMIT" \
+    candidates_path="$candidates_path" \
+    candidate_summary="$candidate_summary" \
+    "$word_collator" |\
   perl -p -n -e 's/ \(.*//' > "$run_output"
   word_splitter_status="${PIPESTATUS[2]} ${PIPESTATUS[3]}"
   cat "$more_warnings" >> "$warning_output"
@@ -1780,6 +1801,23 @@ spelling_body() {
         " | strip_lead)"
       fi
     fi
+    if [ -s "$candidate_summary" ]; then
+      pattern_suggestion_count=$(perl -ne 'next if /^#/;next unless /\S/;print' "$candidate_summary"|wc -l|strip_lead)
+      output_candidate_pattern_suggestions="$(echo "
+        <details><summary>Pattern suggestions :scissors: ($pattern_suggestion_count)</summary>
+
+        You could add these patterns to $new_patterns_file:
+        $B
+        # Automatically suggested patterns
+        $(
+        cat "$candidate_summary"
+        )
+
+        $B
+
+        </details>
+      " | strip_lead)"
+    fi
     if [ -n "$err" ]; then
       output_accept_script="$(echo "
         <details><summary>To accept :heavy_check_mark: these unrecognized words as correct$cleanup_text,
@@ -1802,7 +1840,7 @@ spelling_body() {
     if offer_quote_reply; then
       output_quote_reply_placeholder="$n<!--QUOTE_REPLY-->$n"
     fi
-    OUTPUT=$(echo "$n$report_header$n$OUTPUT$details_note$N$message$extra$output_remove_items$output_excludes$output_excludes_large$output_excludes_suffix$output_accept_script$output_quote_reply_placeholder$output_dictionaries$output_warnings$output_advice
+    OUTPUT=$(echo "$n$report_header$n$OUTPUT$details_note$N$message$extra$output_remove_items$output_excludes$output_excludes_large$output_excludes_suffix$output_accept_script$output_quote_reply_placeholder$output_dictionaries$output_candidate_pattern_suggestions$output_warnings$output_advice
       " | perl -pe 's/^\s+$/\n/;'| uniq)
 }
 

action.yml
@@ -174,7 +174,7 @@ inputs:
   notices:
     description: 'List of events that are notices (items that are neither warnings nor notices will result in an :x:)'
     required: false
-    default: ''
+    default: candidate-pattern
   quit_without_error:
     description: 'Suppress failure code exit code -- it will be available via outputs.result_code'
     required: false

lib/CheckSpelling/SpellingCollator.pm
@@ -15,6 +15,13 @@ sub get_field {
   return $1;
 }
 
+sub get_array {
+  my ($record, $field) = @_;
+  return () unless $record =~ (/\b$field: \[([^\]]+)\]/);
+  my $values = $1;
+  return split /\s*,\s*/, $values;
+}
+
 sub maybe {
   my ($next, $value) = @_;
   $next = $value unless $next && $next < $value;
@@ -103,11 +110,38 @@ sub main {
   my $counter_summary = CheckSpelling::Util::get_file_from_env('counter_summary', '/dev/stderr');
   my $should_exclude_file = CheckSpelling::Util::get_file_from_env('should_exclude_file', '/dev/null');
   my $unknown_word_limit = CheckSpelling::Util::get_val_from_env('unknown_word_limit', undef);
+  my $candidate_summary = CheckSpelling::Util::get_file_from_env('candidate_summary', '/dev/stderr');
 
   open WARNING_OUTPUT, '>:utf8', $warning_output;
   open MORE_WARNINGS, '>:utf8', $more_warnings;
   open COUNTER_SUMMARY, '>:utf8', $counter_summary;
   open SHOULD_EXCLUDE, '>:utf8', $should_exclude_file;
+  open CANDIDATE_SUMMARY, '>:utf8', $candidate_summary;
+
+  my @candidates;
+  if (defined $ENV{'candidates_path'}) {
+    $ENV{'candidates_path'} =~ /(.*)/;
+    if (open CANDIDATES, '<:utf8', $1) {
+      my $candidate_context = '';
+      while (<CANDIDATES>) {
+        my $candidate = $_;
+        if ($candidate =~ /^#/) {
+          $candidate_context .= $candidate;
+          next;
+        }
+        chomp $candidate;
+        unless ($candidate =~ /./) {
+          $candidate_context = '';
+          next;
+        }
+        push @candidates, $candidate_context.$candidate;
+        $candidate_context = '';
+      }
+      close CANDIDATES;
+    }
+  }
+  my @candidate_totals = (0) x scalar @candidates;
+  my @candidate_file_counts = (0) x scalar @candidates;
 
   my @delayed_warnings;
   %letter_map = ();
@@ -154,7 +188,24 @@ sub main {
       $unrecognized=get_field($stats, 'unrecognized');
       $unknown=get_field($stats, 'unknown');
       $unique=get_field($stats, 'unique');
-      #print STDERR "$file (unrecognized: $unrecognized; unique: $unique; unknown: $unknown, words: $words)\n";
+      my @candidate_list;
+      if (@candidate_totals) {
+        @candidate_list=get_array($stats, 'candidates');
+        my @lines=get_array($stats, 'candidate_lines');
+        if (@candidate_list) {
+          for (my $i=0; $i < scalar @candidate_list; $i++) {
+            my $hits = $candidate_list[$i];
+            if ($hits) {
+              $candidate_totals[$i] += $hits;
+              if ($candidate_file_counts[$i]++ < 3) {
+                my $pattern = (split /\n/,$candidates[$i])[-1];
+                push @delayed_warnings, "$file:$lines[$i]:1 ... 1, Notice - Line matches candidate pattern `$pattern` (candidate-pattern)\n";
+              }
+            }
+          }
+        }
+      }
+      #print STDERR "$file (unrecognized: $unrecognized; unique: $unique; unknown: $unknown, words: $words, candidates: [".join(", ", @candidate_list)."])\n";
     }
 
     # These heuristics are very new and need tuning/feedback
@@ -191,6 +242,18 @@ sub main {
     push @directories, $directory;
   }
   close SHOULD_EXCLUDE;
+
+  if (@candidate_totals) {
+    my @indices = sort {
+      $candidate_totals[$b] <=> $candidate_totals[$a] ||
+      $candidate_file_counts[$b] <=> $candidate_file_counts[$a]
+    } 0 .. $#candidate_totals;
+    for my $i (@indices) {
+      last unless $candidate_totals[$i] > 0;
+      print CANDIDATE_SUMMARY "# hit-count: $candidate_totals[$i] file-count: $candidate_file_counts[$i]\n$candidates[$i]\n\n";
+    }
+  }
+  close CANDIDATE_SUMMARY;
 
   if (defined $ENV{'expect'}) {
     $ENV{'expect'} =~ /(.*)/;

lib/CheckSpelling/UnknownWordSplitter.pm
@@ -18,9 +18,10 @@ use File::Temp qw/ tempfile tempdir /;
 use CheckSpelling::Util;
 our $VERSION='0.1.0';
 
-my ($longest_word, $shortest_word, $word_match, $forbidden_re, $patterns_re);
+my ($longest_word, $shortest_word, $word_match, $forbidden_re, $patterns_re, $candidates_re);
 my ($shortest, $longest) = (255, 0);
 my @forbidden_re_list;
+my @candidates_re_list;
 my %dictionary = ();
 my %unique;
 my %unique_unrecognized;
@@ -46,7 +47,7 @@ sub file_to_list {
 sub list_to_re {
   my (@list) = @_;
   return '$^' unless scalar @list;
-  return join "|", @list;
+  return join "|", (grep { $_ ne '' } @list);
 }
 
 sub file_to_re {
@@ -104,9 +105,18 @@ sub load_dictionary {
 sub init {
   my ($dirname) = @_;
   our ($word_match, %unique);
-  our $patterns_re = file_to_re "$dirname/patterns.txt";
+
+  my @patterns_re_list = file_to_list "$dirname/patterns.txt";
+  our $patterns_re = list_to_re @patterns_re_list;
+  my %in_patterns_re_list = map {$_ => 1} @patterns_re_list;
+
   our @forbidden_re_list = file_to_list "$dirname/forbidden.txt";
   our $forbidden_re = list_to_re @forbidden_re_list;
+
+  our @candidates_re_list = file_to_list "$dirname/candidates.txt";
+  @candidates_re_list = map {$in_patterns_re_list{$_} ? '' : $_} @candidates_re_list;
+  our $candidates_re = list_to_re @candidates_re_list;
+
   our $largest_file = CheckSpelling::Util::get_val_from_env('INPUT_LARGEST_FILE', 1024*1024);
 
   $word_match = valid_word();
@@ -159,7 +169,10 @@ sub split_file {
     $unrecognized, $longest_word, $shortest_word, $largest_file, $words,
     $word_match, %unique, %unique_unrecognized, $forbidden_re,
     @forbidden_re_list, $patterns_re, %dictionary,
+    $candidates_re, @candidates_re_list,
   );
+  my @candidates_re_hits = (0) x scalar @candidates_re_list;
+  my @candidates_re_lines = (0) x scalar @candidates_re_list;
   my $temp_dir = tempdir();
   open(NAME, '>:utf8', "$temp_dir/name");
     print NAME $file;
@@ -195,7 +208,9 @@ sub split_file {
     # hook for custom line based text exclusions:
     s/($patterns_re)/"="x length($1)/ge;
     my $previous_line_state = $_;
+    my $line_flagged;
     while (s/($forbidden_re)/"="x length($1)/e) {
+      $line_flagged = 1;
       my ($begin, $end, $match) = ($-[0] + 1, $+[0], $1);
       my $found_trigger_re;
       for my $forbidden_re_singleton (@forbidden_re_list) {
@@ -238,9 +253,27 @@ sub split_file {
       }
       my $after = ($token =~ /[A-Z]$/) ? '(?=[^A-Za-z])|(?=[A-Z][a-z])' : '(?=[^a-z])';
       while ($raw_line =~ /(?:\b|$before)($token)(?:\b|$after)/g) {
+        $line_flagged = 1;
         my ($begin, $end, $match) = ($-[0] + 1, $+[0], $1);
         next unless $match =~ /./;
         print WARNINGS ":$.:$begin ... $end: '$match'\n";
+      }
+    }
+    if ($line_flagged) {
+      $_ = $previous_line_state;
+      s/($candidates_re)/"="x length($1)/ge;
+      if ($_ ne $previous_line_state) {
+        $_ = $previous_line_state;
+        for my $i (0 .. $#candidates_re_list) {
+          my $candidate_re = $candidates_re_list[$i];
+          next unless $candidate_re =~ /./;
+          my $replacements = ($_ =~ s/($candidate_re)/"="x length($1)/ge);
+          if ($replacements) {
+            $candidates_re_hits[$i] += $replacements;
+            $candidates_re_lines[$i] = $. unless $candidates_re_lines[$i];
+            $_ = $previous_line_state;
+          }
+        }
       }
     }
   }
@@ -248,7 +281,11 @@ sub split_file {
 
   if ($unrecognized) {
     open(STATS, '>:utf8', "$temp_dir/stats");
-      print STATS "{words: $words, unrecognized: $unrecognized, unknown: ".(keys %unique_unrecognized).", unique: ".(keys %unique)."}";
+      print STATS "{words: $words, unrecognized: $unrecognized, unknown: ".(keys %unique_unrecognized).
+      ", unique: ".(keys %unique).
+      (@candidates_re_hits ? ", candidates: [".(join ',', @candidates_re_hits)."]" : "").
+      (@candidates_re_lines ? ", candidate_lines: [".(join ',', @candidates_re_lines)."]" : "").
+      "}";
     close STATS;
     open(UNKNOWN, '>:utf8', "$temp_dir/unknown");
       print UNKNOWN join "\n", sort keys %unique_unrecognized;

t/UnknownWordSplitter.t
@@ -10,7 +10,7 @@ use File::Basename;
 use File::Temp qw/ tempfile tempdir /;
 
 use Test::More;
-plan tests => 31;
+plan tests => 37;
 
 use_ok('CheckSpelling::UnknownWordSplitter');
 
@@ -177,3 +177,26 @@ check_output_file_sorted_lines("$output_dir/warnings", ":2:7 ... 19, Warning - `
 :4:5 ... 7: 'ham'
 ");
 check_output_file("$output_dir/unknown", 'ham');
+open $fh, '>', "$dirname/candidates.txt";
+print $fh '# grape
+grape
+
+# pig
+ham
+
+';
+close $fh;
+unlink("$dirname/forbidden.txt");
+CheckSpelling::UnknownWordSplitter::init($dirname);
+open($outputFH, '>', \$output_directory) or die; # This shouldn't fail
+$oldFH = select $outputFH;
+CheckSpelling::UnknownWordSplitter::main($directory, ($filename));
+select $oldFH;
+ok($output_directory =~ /.*\n/);
+chomp($output_directory);
+ok(-d $output_directory);
+check_output_file("$output_directory/name", $filename);
+check_output_file("$output_directory/stats", '{words: 13, unrecognized: 1, unknown: 1, unique: 6, candidates: [0,1], candidate_lines: [0,4]}');
+check_output_file_sorted_lines("$output_directory/warnings", ":4:5 ... 7: 'ham'
+");
+check_output_file("$output_directory/unknown", 'ham');

check-spelling-sandbox/autotest-check-spelling

commit 00ad5afa61bcc64a2c431f7792b7c60a20622ae5

unknown-words: Split error streams
Bug was introduced in https://github.com/check-spelling-sandbox/check-spelling/commit/2d44fce812639aef62fc025e9520c14be61b397a

It was incompatible with a change from https://github.com/check-spelling-sandbox/check-spelling/commit/12da0761985433deb1a4d01c58d13b0af16c46c3

unknown-words.sh
@@ -2065,7 +2065,10 @@ set_up_files() {
     spellchecker="$spellchecker" \
     "$word_splitter" 2> /dev/null |
     early_warnings="$early_warnings.1" \
-    INPUT_USE_SARIF='' INPUT_DISABLE_CHECKS=noisy-file "$word_collator" 2> "$expect_notes" > "$expect_collated"
+    counter_summary=/dev/null \
+    more_warnings=/dev/null \
+    warning_output="$expect_notes" \
+    INPUT_USE_SARIF='' INPUT_DISABLE_CHECKS=noisy-file "$word_collator" > "$expect_collated"
     perl -pe 's/ \(.*\)//' "$expect_collated" > "$expect_path"
     "$expect_collator" "$expect_collated" "$expect_notes" >> "$early_warnings"
   else