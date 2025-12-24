1573a956b87de92ca05a373d1e88a85c897c61b7
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-12-24 06:41:14 -0500

    LoadEnv: rename input_map to inputs

--- load-env.pl
+++ load-env.pl
@@ -4,8 +4,8 @@ use CheckSpelling::LoadEnv;
 
 my $parsed_inputs = CheckSpelling::LoadEnv::parse_inputs();
 
-my %input_map = %{$parsed_inputs->{'input_map'}};
-for my $var (sort keys %input_map) {
-    next if $var eq 'INTERNAL_STATE_DIRECTORY' && $input_map{$var} eq '';
-    CheckSpelling::LoadEnv::print_var_val($var, $input_map{$var});
+my %inputs = %{$parsed_inputs->{'inputs'}};
+for my $var (sort keys %inputs) {
+    next if $var eq 'INTERNAL_STATE_DIRECTORY' && $inputs{$var} eq '';
+    CheckSpelling::LoadEnv::print_var_val($var, $inputs{$var});
 }
