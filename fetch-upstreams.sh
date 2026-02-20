3aab0eb51167d48f134959905de6c2909abcc089
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-20 09:15:06 -0500

    Also backport autodetect-expired-artifact-3

--- fetch-upstreams.sh
+++ fetch-upstreams.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 git_remote=$(git config --get remote.origin.url)
 extra_workflows=$(cd ../workflows; pwd)
-git fetch origin autodetect-expired-artifact-2:autodetect-expired-artifact
+cherry_pick_start=2
+cherry_pick_stop=3
+for cherry_pick in $(seq $cherry_pick_start $cherry_pick_stop); do
+  git fetch origin autodetect-expired-artifact-$cherry_pick:autodetect-expired-artifact-$cherry_pick
+done
+
 for origin in $origins; do
   export origin
   git remote add upstream https://github.com/$origin/check-spelling.git --no-tags
@@ -12,17 +17,20 @@ for origin in $origins; do
     git fetch upstream $branch:$local
     if [ "$branch" = main ]; then
       git checkout $local
-      if git cherry-pick autodetect-expired-artifact; then
-        git commit --amend -m "$(
-          echo 'cherry-pick fix for expired artifacts'
-          echo
-          echo https://github.com/check-spelling/check-spelling/commit/$(git rev-parse autodetect-expired-artifact)
-        )"
-        echo "::notice title=Backported autodetect-expired-artifact::To $local"
-      else
-        git cherry-pick --abort
-        git clean -x
-      fi
+      for cherry_pick in $(seq $cherry_pick_start $cherry_pick_stop); do
+        fix=autodetect-expired-artifact-$cherry_pick
+        if git cherry-pick $fix; then
+          git commit --amend -m "$(
+            echo "cherry-pick fix $cherry_pick for expired artifacts"
+            echo
+            echo https://github.com/check-spelling/check-spelling/commit/$(git rev-parse $fix)
+          )"
+          echo "::notice title=Backported $fix::To $local"
+        else
+          git cherry-pick --abort
+          git clean -x
+        fi
+      done
     fi
     git push origin $local -f
 
