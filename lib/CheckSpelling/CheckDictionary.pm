918b9187b3d3015b2abaa1fcda2d7a43779412af
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-07-13 17:26:30 -0400

    CheckDictionary: Report non-alpha-in-dictionary characters

--- lib/CheckSpelling/CheckDictionary.pm
+++ lib/CheckSpelling/CheckDictionary.pm
@@ -1,6 +1,7 @@
 #! -*-perl-*-
 
 package CheckSpelling::CheckDictionary;
+use CheckSpelling::Util;
 
 sub process_line {
     my ($line) = @_;
@@ -8,8 +9,9 @@ sub process_line {
     my $ignore_pattern = $ENV{INPUT_IGNORE_PATTERN} || '';
     if ($ignore_pattern ne '' && $line =~ /^.*?($ignore_pattern+)/) {
         my ($left, $right) = ($-[1] + 1, $+[1] + 1);
+        my $wrapped = CheckSpelling::Util::wrap_in_backticks($1);
         my $column_range="$left ... $right";
-        return ('', "$column_range, Warning - Ignoring entry because it contains non-alpha characters (non-alpha-in-dictionary)\n");
+        return ('', "$column_range, Warning - Ignoring entry because it contains non-alpha characters - $wrapped (non-alpha-in-dictionary)\n");
     }
     return ($line, '');
 }
--- t/unknown-words/output/summary.md
+++ t/unknown-words/output/summary.md
@@ -185,7 +185,7 @@ note|path
 
 note|path
 -|-
-Ignoring entry because it contains non-alpha characters | GITHUB_SERVER_URL/GITHUB_REPOSITORY_OWNER/GITHUB_REPOSITORY_NAME/blame/GITHUB_SHA/t/unknown-words/config/expect/expect.txt#L1
+Ignoring entry because it contains non-alpha characters - `+` | GITHUB_SERVER_URL/GITHUB_REPOSITORY_OWNER/GITHUB_REPOSITORY_NAME/blame/GITHUB_SHA/t/unknown-words/config/expect/expect.txt#L1
 </details>
 
 <details><summary>📂 unrecognized-spelling</summary>
