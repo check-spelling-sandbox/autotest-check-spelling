b44eb688190e693e93693189d1118736a67b205e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-07-14 21:40:15 -0400

    CheckDictionary: Fix handling of environment variables

--- lib/CheckSpelling/CheckDictionary.pm
+++ lib/CheckSpelling/CheckDictionary.pm
@@ -3,10 +3,15 @@
 package CheckSpelling::CheckDictionary;
 use CheckSpelling::Util;
 
+sub init {
+   our $comment_char = CheckSpelling::Util::get_file_from_env_utf8('comment_char');
+   our $ignore_pattern = CheckSpelling::Util::get_file_from_env_utf8('INPUT_IGNORE_PATTERN') || '';
+}
+
 sub process_line {
     my ($line) = @_;
-    $line =~ s/$ENV{comment_char}.*//;
-    my $ignore_pattern = $ENV{INPUT_IGNORE_PATTERN} || '';
+    our ($comment_char, $ignore_pattern);
+    $line =~ s/$comment_char.*//;
     if ($ignore_pattern ne '' && $line =~ /^.*?($ignore_pattern+)/) {
         my ($left, $right) = ($-[1] + 1, $+[1] + 1);
         my $wrapped = CheckSpelling::Util::wrap_in_backticks($1);
